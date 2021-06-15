dbdd28bc3e73d8e4d8c275c1972eda53119e6539
Lewis Baumstark <lewisb@westga.edu>
2021-06-15 14:20:10 -0400

    properties bound and defaults set for VIC-II bank select and programming language select

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,6 +25,7 @@ import {MatDividerModule} from '@angular/material/divider';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatGridListModule,
     MatCardModule,
     MatFormFieldModule,
--- src/app/memmap-configurator/memmap-configurator.component.ts
+++ src/app/memmap-configurator/memmap-configurator.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemmapConfiguratorComponent implements OnInit {
 
+  vicBank: number = 0;
+  programmingLanguage: string = "ASM";
+  
   constructor() { }
 
   ngOnInit(): void {
