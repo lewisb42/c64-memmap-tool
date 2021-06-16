a46c2b7759d57881ec4cb3af6a269b7a43142bb4
Lewis Baumstark <lewisb@westga.edu>
2021-06-16 16:15:36 -0400

    first success at dynamic update of a bank

--- src/app/memmap-configurator/memmap-configurator.component.ts
+++ src/app/memmap-configurator/memmap-configurator.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 import { GoogleChartInterface } from 'ng2-google-charts';
+import { MemoryChunk, MemoryStatus } from '../models/memory-chunk';
+import { MemoryBank } from '../models/memory-bank';
 
 function toAddress(x: number): string {
   let baseHex = x.toString(16).toUpperCase();
@@ -17,13 +19,13 @@ function toAddress(x: number): string {
   templateUrl: './memmap-configurator.component.html',
   styleUrls: ['./memmap-configurator.component.scss']
 })
-export class MemmapConfiguratorComponent implements OnInit, OnChanges {
+export class MemmapConfiguratorComponent implements OnInit {
 
-public memChart: GoogleChartInterface = {
+private memChartPrototype: GoogleChartInterface = {
     chartType: 'BarChart',
     dataTable: [
-      [ 'desc', 'Zero Page', 'Stack', 'data section', 'code section', { role: 'annotation' } ],
-      ['VIC Bank 0', 256, 256, 1536, 14335, '' ]
+      [ 'desc', 'free space', { role: 'annotation' } ],
+      ['VIC Bank 0', 16384, '' ]
     ],
     options: {
       //width: 600,
@@ -38,27 +40,69 @@ public memChart: GoogleChartInterface = {
       }
     }
   };
+  
+  public memChart: GoogleChartInterface = { ...this.memChartPrototype };
 
   vicBank: number = 0;
   programmingLanguage: string = 'ASM';
-  useKernelRom: boolean = true;
-  useBasicRom: boolean = true;
+  useKernelRom: boolean = false;
+  useBasicRom: boolean = false;
   useCartRomLo: boolean = false;
   cartRomHi: string = "unmapped";
   dBankMap: string = "IO";
   
+  vicBank0_dataTable: Object[] = [
+      [ 'desc', 'free space', { role: 'annotation' } ],
+      ['VIC Bank 0', 16384, '' ]
+    ];
+  
   basicMode: boolean = false;
   
+  private zeroPageChunk: MemoryChunk = new MemoryChunk('zero page', 0x000, 256, MemoryStatus.UNAVAILABLE);
+  private stackChunk: MemoryChunk = new MemoryChunk('stack page', 0x0100, 256, MemoryStatus.UNAVAILABLE);
   constructor() { }
 
   ngOnInit(): void {
   }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    
-    console.log("cartRomHi: " + this.cartRomHi);
-  }
  
+  private configureVicBank0(): void {
+    var bank = new MemoryBank('VIC Bank 0', 0);
+    
+    // kernel & basic use the zero page for stuff
+    if (this.useBasicRom || this.useKernelRom) {
+      bank.insertChunk(this.zeroPageChunk);
+    }
+    
+    // always leave the stack alone
+    bank.insertChunk(this.stackChunk);
+    
+    this.updateChart(bank);
+  }
+  
+  private updateChart(bank:MemoryBank) {
+    //var chart: GoogleChartInterface = { ...this.memChartPrototype };
+    var dataTable = this.formatAsDataTable(bank);
+    this.memChart.dataTable = dataTable;
+    this.memChart.component!.draw();
+    //this.memChart.wrapper.setDataTable(this.formatAsDataTable(bank));
+    //this.memChart.redraw();
+  }
+  
+  private formatAsDataTable(bank: MemoryBank): Object[] {
+    var labels: Object[] = [ 'desc' ];
+    var values: Object[] = [ bank.name ];
+    var chunks = bank.chunks;
+    
+    for (let chunk of chunks) {
+      values.push(chunk.sizeInBytes);
+      labels.push(chunk.desc);
+    }
+    
+    labels.push( {role: 'annotation' } );
+    values.push('');
+    
+    return [ labels, values ];
+  }
   
   onBasicModeSelected(): void {
     if (this.cartRomHi == 'bankA') {
@@ -67,7 +111,8 @@ public memChart: GoogleChartInterface = {
     this.useBasicRom = true;
     this.useKernelRom = true;
     this.basicMode = true;
-    console.log("cartRomHi: " + this.cartRomHi);
+    
+    this.configureVicBank0();
   }
   
   onAssemblyModeSelected(): void {
