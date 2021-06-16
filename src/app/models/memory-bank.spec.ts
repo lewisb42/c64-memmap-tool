977164ab1a6590cbef61211500a588c0049609fc
Lewis Baumstark <lewisb@westga.edu>
2021-06-16 15:24:04 -0400

    insertChunk() tests: insert multiple chunks into the interior of a bank

--- src/app/models/memory-bank.spec.ts
+++ src/app/models/memory-bank.spec.ts
@@ -96,4 +96,82 @@ describe('Inserting chunks into a bank', () => {
         expect(chunk3.startAddr).toBe(110);
         expect(chunk3.sizeInBytes).toBe(16426);
     });
+    
+    it('should insert contiguous chunks', () => {
+        let bank = new MemoryBank('joe', 0);
+        
+        let chunkA = new MemoryChunk('', 100, 10, MemoryStatus.UNAVAILABLE);
+        let result = bank.insertChunk(chunkA);
+        expect(result.success).toBeTruthy(result.reason);
+        
+        let chunkB = new MemoryChunk('', 110, 10, MemoryStatus.UNAVAILABLE);
+        result = bank.insertChunk(chunkB);
+        expect(result.success).toBeTruthy(result.reason);
+        
+        let chunks = bank.chunks;
+        expect(chunks.length).toBe(4);
+        
+        let chunk1 = chunks[0];
+        expect(chunk1.startAddr).toBe(0);
+        expect(chunk1.sizeInBytes).toBe(100);
+        
+        let chunk2 = chunks[1];
+        expect(chunk2.startAddr).toBe(100);
+        expect(chunk2.sizeInBytes).toBe(10);
+        
+        let chunk3 = chunks[2];
+        expect(chunk3.startAddr).toBe(110);
+        expect(chunk3.sizeInBytes).toBe(10);
+        
+        let chunk4 = chunks[3];
+        expect(chunk4.startAddr).toBe(120);
+        expect(chunk4.sizeInBytes).toBe(16416);
+    });
+    
+    it('should insert several chunks', () => {
+        let bank = new MemoryBank('joe', 0);
+        
+        let chunkA = new MemoryChunk('', 100, 10, MemoryStatus.UNAVAILABLE);
+        let result = bank.insertChunk(chunkA);
+        expect(result.success).toBeTruthy(result.reason);
+        
+        let chunkB = new MemoryChunk('', 120, 10, MemoryStatus.UNAVAILABLE);
+        result = bank.insertChunk(chunkB);
+        expect(result.success).toBeTruthy(result.reason);
+        
+        let chunkC = new MemoryChunk('', 140, 10, MemoryStatus.UNAVAILABLE);
+        result = bank.insertChunk(chunkC);
+        expect(result.success).toBeTruthy(result.reason);
+        
+        let chunks = bank.chunks;
+        expect(chunks.length).toBe(7);
+        
+        let chunk1 = chunks[0];
+        expect(chunk1.startAddr).toBe(0);
+        expect(chunk1.sizeInBytes).toBe(100);
+        
+        let chunk2 = chunks[1];
+        expect(chunk2.startAddr).toBe(100);
+        expect(chunk2.sizeInBytes).toBe(10);
+        
+        let chunk3 = chunks[2];
+        expect(chunk3.startAddr).toBe(110);
+        expect(chunk3.sizeInBytes).toBe(10);
+        
+        let chunk4 = chunks[3];
+        expect(chunk4.startAddr).toBe(120);
+        expect(chunk4.sizeInBytes).toBe(10);
+        
+        let chunk5 = chunks[4];
+        expect(chunk5.startAddr).toBe(130);
+        expect(chunk5.sizeInBytes).toBe(10);
+        
+        let chunk6 = chunks[5];
+        expect(chunk6.startAddr).toBe(140);
+        expect(chunk6.sizeInBytes).toBe(10);
+        
+        let chunk7 = chunks[6];
+        expect(chunk7.startAddr).toBe(150);
+        expect(chunk7.sizeInBytes).toBe(16386);
+    });
 });
